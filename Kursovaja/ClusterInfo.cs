d48f59fe5dc31e90bd1b258cd916a5479f2d674b
IrinaSemak <irinasemak07@gmail.com>
2025-04-10 11:10:48 +0300

    Редактирование функционала и исправление ошибок в программе

---
+++ Kursovaja/ClusterInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace Library
+{
+    public class ClusterInfo
+    {
+        public int ClusterId { get; set; }
+        public int PointCount { get; set; }
+        public Brush ClusterColor { get; set; }
+    }
+}
